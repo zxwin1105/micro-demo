c4baee478402d260cf15446310ec377480a7e7bd
翟鑫伟 <zxwinnn@163.com>
2022-08-29 16:26:26 +0800

    整合swagger2

--- account-server/src/main/java/com/micro/account/AccountApplication.java
+++ account-server/src/main/java/com/micro/account/AccountApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author zhaixinwei
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableSwagger2
 public class AccountApplication {
 
     public static void main(String[] args) {
--- account-server/src/main/java/com/micro/account/controller/TestController.java
+++ account-server/src/main/java/com/micro/account/controller/TestController.java
@@ -1,25 +1,32 @@
 package com.micro.account.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apiguardian.api.API;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author zhaixinwei
  * @date 2022/8/25
  * @description
  */
-//@RestController
-@RefreshScope
+
+@Api(tags = "TestController（测试）",value = "测试控制器")
+@RestController
 @RequestMapping("/test")
 public class TestController {
-    @Value("${test.config}")
-    private String config;
 
+    @ApiOperation(value = "测试")
     @GetMapping
     public String test(){
-        return config;
+        return LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE);
     }
 }
